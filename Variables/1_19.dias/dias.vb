513aefa6f3a5ec25ab05a14575c0f420b88e87f1
Facundo Vergara <fakumax@gmail.com>
2019-05-06 22:38:28 -0300

    V1.2

---
+++ Variables/1_19.dias/dias.vb
@@ -0,0 +1,26 @@
+Imports System
+'1.19. Crear un proyecto y un módulo llamado “días” utilizando una enumeración para los días
+'de la semana en español, mostrar que día que corresponde al resultado.}
+
+Enum semanaDias
+    Lunes
+    Martes
+    Miercoles
+    Jueves
+    Viernes
+    Sabado
+    Domingo
+End Enum
+
+Module dias
+    Sub Main(args As String())
+        Console.WriteLine("{0} - valor: {1}", semanaDias.Lunes.ToString, semanaDias.Lunes.GetHashCode)
+        Console.WriteLine("{0} - valor: {1}", semanaDias.Martes.ToString, semanaDias.Martes.GetHashCode)
+        Console.WriteLine("{0} - valor: {1}", semanaDias.Miercoles.ToString, semanaDias.Miercoles.GetHashCode)
+        Console.WriteLine("{0} - valor: {1}", semanaDias.Jueves.ToString, semanaDias.Jueves.GetHashCode)
+        Console.WriteLine("{0} - valor: {1}", semanaDias.Viernes.ToString, semanaDias.Viernes.GetHashCode)
+        Console.WriteLine("{0} - valor: {1}", semanaDias.Sabado.ToString, semanaDias.Sabado.GetHashCode)
+        Console.WriteLine("{0} - valor: {1}", semanaDias.Domingo.ToString, semanaDias.Domingo.GetHashCode)
+
+    End Sub
+End Module
